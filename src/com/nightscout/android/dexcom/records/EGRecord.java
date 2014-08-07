1b3c0898d717927a89bb58ac9a742d48ff567250
rnpenguin <rosshnaylor@gmail.com>
2014-08-07 00:35:21 -0700

    Adding new EGRecord class, still needs some work.

---
+++ src/com/nightscout/android/dexcom/records/EGRecord.java
@@ -0,0 +1,36 @@
+package com.nightscout.android.dexcom.records;
+
+import com.nightscout.android.dexcom.Constants;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class EGRecord implements Serializable {
+
+    private int displayTime;
+    private int bGValue;
+    private String trend = "---";
+
+    public EGRecord(byte[] packet) {
+        // uint, uint, ushort, byte, ushort
+        // (system_time, display_time, glucose, trend_arrow, crc)
+        // TODO: convert time using epoch
+        displayTime = ByteBuffer.wrap(packet).order(ByteOrder.LITTLE_ENDIAN).getInt(4);
+        int eGValue = ByteBuffer.wrap(packet).order(ByteOrder.LITTLE_ENDIAN).getShort(8);
+        bGValue = eGValue & Constants.EGV_VALUE_MASK;
+//        trend = Constants.TREND_ARROW_VALUES.values()[ByteBuffer.wrap(packet).get(10)].friendlyTrendName();
+    }
+
+    public int getDisplayTime() {
+        return displayTime;
+    }
+
+    public int getBGValue() {
+        return bGValue;
+    }
+
+    public String getTrend() {
+        return trend;
+    }
+}
