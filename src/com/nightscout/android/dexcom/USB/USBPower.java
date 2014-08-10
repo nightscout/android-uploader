d2cbc15ba1044e0ed0abf816509a71397e4dcf35
Domokun14 <hackingtype1@gmail.com>
2014-08-09 23:22:55 -0400

    Handles device specific (ST18i) commands in line with standard command process.

--- src/com/nightscout/android/dexcom/USB/USBPower.java
+++ src/com/nightscout/android/dexcom/USB/USBPower.java
@@ -1,11 +1,13 @@
 package com.nightscout.android.dexcom.USB;
 
+import android.os.Build;
 import android.util.Log;
+
 import com.nightscout.android.dexcom.DexcomG4Activity;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.FileReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class USBPower {
@@ -14,33 +16,36 @@ public class USBPower {
 
     private static final String SET_POWER_ON_COMMAND = "echo 'on' > \"/sys/bus/usb/devices/1-1/power/level\"";
     private static final String SET_POWER_SUSPEND_COMMAND_A = "echo \"0\" > \"/sys/bus/usb/devices/1-1/power/autosuspend\"";
-    private static final String SET_POWER_SUSPEND_COMMAND_B = "echo \"suspend\" > \"/sys/bus/usb/devices/1-1/power/level\"";
+    private static final String SET_POWER_SUSPEND_COMMAND_B = "echo \"auto\" > \"/sys/bus/usb/devices/1-1/power/level\"";
+    
     private static final String ENABLE_COMMAND = "echo \"1\" > \"/sys/bus/usb/devices/1-1/bConfigurationValue\"";
     private static final String ENABLE_FILE = "/sys/bus/usb/devices/1-1/bConfigurationValue";
-
+    
     private static String readFile(String filename) {
-      String retVal = null;
-      try {
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        retVal = br.readLine();
-      } catch (Exception e) {
-        Log.e(TAG, e.getLocalizedMessage() + "");
-      }
-      return retVal;
-    }
-
-    public static void enable() {
-        String status = readFile(ENABLE_FILE);
-        if (status == null) {
-            try {
-                runCommand(ENABLE_COMMAND);
-                Log.i(TAG, "Enabled USB device");
-            } catch (Exception e) {
-                Log.e(TAG, "USB device already enabled");
-            }
+        String retVal = null;
+        try {
+          BufferedReader br = new BufferedReader(new FileReader(filename));
+          retVal = br.readLine();
+        } catch (Exception e) {
+          Log.e(TAG, e.getLocalizedMessage() + "");
         }
-    }
+        return retVal;
+      }
 
+      public static void enable() {
+          String status = readFile(ENABLE_FILE);
+          if (status == null) {
+              try {
+                  runCommand(ENABLE_COMMAND);
+                  Log.i(TAG, "Enabled USB device");
+              } catch (Exception e) {
+                  Log.e(TAG, "USB device already enabled");
+              }
+          }
+      }
+    
+    
+    
     public static void PowerOff() {
         try {
             runCommand(SET_POWER_SUSPEND_COMMAND_A);
@@ -53,7 +58,13 @@ public class USBPower {
 
     public static void PowerOn(){
         try {
-            enable();
+        	Log.e(TAG, Build.MODEL);
+        	if(Build.MODEL.equalsIgnoreCase("ST18i"))
+        	{
+        		
+        		enable();
+        	}
+        	
             runCommand(SET_POWER_ON_COMMAND);
             Log.i(TAG, "PowerOn USB complete");
         } catch (Exception e) {
