75b235b5e1a7bf1c1601157520eab5461011c921
rnpenguin <rosshnaylor@gmail.com>
2014-09-01 18:25:20 -0700

    Added latest page of EGV and MBG record and broadcast back to UI.

--- app/src/main/java/com/nightscout/android/dexcom/SyncingService.java
+++ app/src/main/java/com/nightscout/android/dexcom/SyncingService.java
@@ -6,8 +6,12 @@ import android.content.Context;
 import android.hardware.usb.UsbManager;
 import android.util.Log;
 
+import com.nightscout.android.MainActivity;
 import com.nightscout.android.dexcom.USB.UsbSerialDriver;
 import com.nightscout.android.dexcom.USB.UsbSerialProber;
+import com.nightscout.android.dexcom.records.EGRecord;
+import com.nightscout.android.dexcom.records.MeterRecord;
+import com.nightscout.android.upload.Uploader;
 
 import java.io.IOException;
 
@@ -19,11 +23,17 @@ public class SyncingService extends IntentService {
 
     private static final String ACTION_SYNC = "com.nightscout.android.dexcom.action.SYNC";
 
-    private static final String EXTRA_PARAM1 = "com.nightscout.android.dexcom.extra.2DAY";
-    private static final String EXTRA_PARAM2 = "com.nightscout.android.dexcom.extra.SINGLE";
+    private static final String TWO_DAY = "com.nightscout.android.dexcom.extra.2DAY";
+    private static final String SINGLE = "com.nightscout.android.dexcom.extra.SINGLE";
+
+    // Response to broadcast to activity
+    public static final String RESPONSE_SGV = "mySGV";
+    public static final String RESPONSE_TIMESTAMP = "myTimestamp";
+    public static final String RESPONSE_NEXT_UPLOAD_TIME = "myUploadTime";
 
     private final String TAG = SyncingService.class.getSimpleName();
 
+    private Context mContext;
     private UsbManager mUsbManager;
     private UsbSerialDriver mSerialDevice;
 
@@ -36,7 +46,7 @@ public class SyncingService extends IntentService {
     public static void startActionSync(Context context, String param1, String param2) {
         Intent intent = new Intent(context, SyncingService.class);
         intent.setAction(ACTION_SYNC);
-        intent.putExtra(EXTRA_PARAM2, param1);
+        intent.putExtra(SINGLE, param1);
         context.startService(intent);
     }
 
@@ -46,10 +56,11 @@ public class SyncingService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        mContext = getApplicationContext();
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_SYNC.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param1 = intent.getStringExtra(TWO_DAY);
                 handleActionSync(param1);
             }
         }
@@ -62,21 +73,39 @@ public class SyncingService extends IntentService {
     private void handleActionSync(String param1) {
         acquireSerialDevice();
         ReadData readData = new ReadData(mSerialDevice);
-        boolean test = readData.ping();
+        EGRecord[] recentRecords = readData.getRecentEGVs();
+        MeterRecord[] meterRecords = readData.getRecentMeterRecords();
+        Uploader uploader = new Uploader(mContext);
+        uploader.upload(recentRecords, meterRecords);
+
+        EGRecord recentEGV = recentRecords[recentRecords.length - 1];
+        MeterRecord recentMeterBG = meterRecords[meterRecords.length - 1];
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(MainActivity.CGMStatusReceiver.PROCESS_RESPONSE);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        broadcastIntent.putExtra(RESPONSE_SGV, String.valueOf(recentEGV.getBGValue()) + " "
+                + recentEGV.getTrendSymbol());
+        broadcastIntent.putExtra(RESPONSE_TIMESTAMP, recentEGV.getDisplayTime().toString());
+        broadcastIntent.putExtra(RESPONSE_NEXT_UPLOAD_TIME, 60000);
+        sendBroadcast(broadcastIntent);
     }
 
-    private void acquireSerialDevice() {
+    // TODO: this needs to be more robust as before, but will clean up it and implement here, this
+    // is just simple testing code
+    private boolean acquireSerialDevice() {
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
         mSerialDevice = UsbSerialProber.acquire(mUsbManager);
         if (mSerialDevice != null) {
             try {
                 mSerialDevice.open();
+                return true;
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
             Log.d(TAG, "Unable to acquire USB device from manager.");
         }
+        return false;
     }
 
 }
