3fc4c8d8948b62a94708fbd30b03808310a8d8e7
rnpenguin <rosshnaylor@gmail.com>
2014-09-03 22:13:51 -0700

    Adding Utils class with static method for converting receiver time to Date object.

--- app/src/main/java/com/nightscout/android/dexcom/records/GenericTimestampRecord.java
+++ app/src/main/java/com/nightscout/android/dexcom/records/GenericTimestampRecord.java
@@ -1,26 +1,23 @@
 package com.nightscout.android.dexcom.records;
 
+import com.nightscout.android.dexcom.Utils;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
 public class GenericTimestampRecord {
 
-    public final Date EPOCH = new GregorianCalendar(2009, 0, 1).getTime();
     private final int OFFSET_SYS_TIME = 0;
     private final int OFFSET_DISPLAY_TIME = 4;
-    private int currentTZOffset = TimeZone.getDefault().getRawOffset();
-    private long epochMS = 1230768000000L;
     private Date systemTime;
     private Date displayTime;
 
     public GenericTimestampRecord(byte[] packet) {
         int st = ByteBuffer.wrap(packet).order(ByteOrder.LITTLE_ENDIAN).getInt(OFFSET_SYS_TIME);
-        systemTime = getDate(st);
+        systemTime = Utils.receiverTimeToDate(st);
         int dt = ByteBuffer.wrap(packet).order(ByteOrder.LITTLE_ENDIAN).getInt(OFFSET_DISPLAY_TIME);
-        displayTime = getDate(dt);
+        displayTime = Utils.receiverTimeToDate(dt);
     }
 
     public Date getSystemTime() {
@@ -31,13 +28,4 @@ public class GenericTimestampRecord {
         return displayTime;
     }
 
-    // TODO: this will be used in 1 other place, thus might be best to get in a utilities class
-    private Date getDate(int receiverTime) {
-        // TODO: probably best to do this Adriens way, TBD
-        long milliseconds = epochMS - currentTZOffset;
-        long timeAdd = milliseconds + (1000L * receiverTime);
-        TimeZone tz = TimeZone.getDefault();
-        if (tz.inDaylightTime(new Date())) timeAdd = timeAdd - 3600000L;
-        return new Date(timeAdd);
-    }
 }
