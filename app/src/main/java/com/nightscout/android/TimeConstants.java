79a7306a12f8e95eab061366071293d1d8a777a4
rnpenguin <rosshnaylor@gmail.com>
2014-09-17 22:56:40 -0700

    Fixed FIVE_MINUTES_MS.

--- app/src/main/java/com/nightscout/android/TimeConstants.java
+++ app/src/main/java/com/nightscout/android/TimeConstants.java
@@ -2,7 +2,7 @@ package com.nightscout.android;
 
 public class TimeConstants {
 
-    public final static int FIVE_MINUTES_MS = 300;
+    public final static int FIVE_MINUTES_MS = 300000;
     public final static int SEC_TO_MS = 1000;
 
 }
