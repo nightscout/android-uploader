2100c983190e2cfdd1e22af984a94534d01294d7
rnpenguin <rosshnaylor@gmail.com>
2014-09-20 22:56:55 -0700

    Added icon to feedback dialog.

--- app/src/main/java/com/nightscout/android/Nightscout.java
+++ app/src/main/java/com/nightscout/android/Nightscout.java
@@ -15,7 +15,7 @@ import org.acra.annotation.*;
         formUriBasicAuthPassword="8CgjF6r2u4i8EhPHoPJjnk8f",
         resToastText = R.string.crash_toast_text,
         resDialogText = R.string.feebback_dialog_text,
-        resDialogIcon = android.R.drawable.ic_dialog_alert,
+        resDialogIcon = R.drawable.ic_launcher,
         resDialogTitle = R.string.feedback_dialog_title,
         resDialogCommentPrompt = R.string.feedback_dialog_comment_prompt,
         resDialogOkToast = R.string.feedback_dialog_ok_toast,
