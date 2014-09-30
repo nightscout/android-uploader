6bcbc0dcd3ddf521c5e6290becf2cd7c35a45c86
rnpenguin <rosshnaylor@gmail.com>
2014-09-29 21:40:25 -0700

    Cleaned up getTimeString method.

--- app/src/main/java/com/nightscout/android/dexcom/Utils.java
+++ app/src/main/java/com/nightscout/android/dexcom/Utils.java
@@ -16,27 +16,28 @@ public class Utils {
     }
 
     public static String getTimeString(long timeDeltaMS) {
-        long minutes=(timeDeltaMS/1000)/60;
-        long hours=(minutes/60);
-        long days=(hours/24);
-        long weeks=(days/7);
-        minutes=minutes-(hours*60);
-        hours=hours-(days*24);
-        days=days-(weeks*7);
+        long minutes = (timeDeltaMS / 1000) / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+        long weeks = days / 7;
+        minutes= minutes - hours * 60;
+        hours = hours - days * 24;
+        days= days - weeks * 7;
 
         String timeAgoString="";
-        if (weeks>0){
+        if (weeks > 0) {
             timeAgoString+=weeks+" weeks ";
         }
-        if (days>0){
+        if (days > 0) {
             timeAgoString+=days+" days ";
         }
-        if (hours>0){
+        if (hours > 0) {
             timeAgoString+=hours+" hours ";
         }
-        if (minutes>=0){
-            timeAgoString+=minutes+" minutes ";
+        if (minutes >= 0) {
+            timeAgoString+=minutes+" min ";
         }
-        return (timeAgoString.equals("")?"--":timeAgoString+"ago");
+
+        return (timeAgoString.equals("") ? "--" : timeAgoString + "ago");
     }
 }
