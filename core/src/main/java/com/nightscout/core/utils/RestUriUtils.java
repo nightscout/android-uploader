c70fe903961b0999a9eb0c39ed4f291cc264d387
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2015-06-15 21:01:30 -0700

    Fix NPE in RestUriUtils.
    Tested:
      Unit test.

--- core/src/main/java/com/nightscout/core/utils/RestUriUtils.java
+++ core/src/main/java/com/nightscout/core/utils/RestUriUtils.java
@@ -14,41 +14,43 @@ import static net.tribe7.common.base.Preconditions.checkArgument;
 import static net.tribe7.common.base.Preconditions.checkNotNull;
 
 public class RestUriUtils {
-    public static boolean isV1Uri(URI uri) {
-        return uri != null && (uri.getPath().endsWith("v1") || uri.getPath().endsWith("v1/"));
-    }
 
-    public static boolean hasToken(URI uri) {
-        return !Strings.isNullOrEmpty(uri.getUserInfo());
-    }
+  public static boolean isV1Uri(URI uri) {
+    return uri != null && uri.getPath() != null &&
+           (uri.getPath().endsWith("v1") || uri.getPath().endsWith("v1/"));
+  }
 
-    /**
-     * Removes the token from the uri.
-     *
-     * @param uri Non-null uri to strip the token from.
-     * @return uri without token.
-     */
-    public static URI removeToken(URI uri) {
-        checkNotNull(uri);
-        // This is gross, but I don't know a better way to do it.
-        return URI.create(uri.toString().replaceFirst("//[^@]+@", "//"));
-    }
+  public static boolean hasToken(URI uri) {
+    return !Strings.isNullOrEmpty(uri.getUserInfo());
+  }
 
-    /**
-     * Generates a secret from the given token.
-     *
-     * @param secret Non-null, non-empty secret to generate the token from.
-     * @return The generated token.
-     */
-    public static String generateSecret(String secret) {
-        checkArgument(!Strings.isNullOrEmpty(secret));
-        return Hashing.sha1().hashBytes(secret.getBytes(Charsets.UTF_8)).toString();
-    }
+  /**
+   * Removes the token from the uri.
+   *
+   * @param uri Non-null uri to strip the token from.
+   * @return uri without token.
+   */
+  public static URI removeToken(URI uri) {
+    checkNotNull(uri);
+    // This is gross, but I don't know a better way to do it.
+    return URI.create(uri.toString().replaceFirst("//[^@]+@", "//"));
+  }
 
-    public static List<String> splitIntoMultipleUris(String combinedUris) {
-        if (Strings.isNullOrEmpty(combinedUris)) {
-            return new ArrayList<>();
-        }
-        return Splitter.onPattern("\\s+").splitToList(combinedUris.trim());
+  /**
+   * Generates a secret from the given token.
+   *
+   * @param secret Non-null, non-empty secret to generate the token from.
+   * @return The generated token.
+   */
+  public static String generateSecret(String secret) {
+    checkArgument(!Strings.isNullOrEmpty(secret));
+    return Hashing.sha1().hashBytes(secret.getBytes(Charsets.UTF_8)).toString();
+  }
+
+  public static List<String> splitIntoMultipleUris(String combinedUris) {
+    if (Strings.isNullOrEmpty(combinedUris)) {
+      return new ArrayList<>();
     }
+    return Splitter.onPattern("\\s+").splitToList(combinedUris.trim());
+  }
 }
