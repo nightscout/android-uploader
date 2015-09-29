40af5131b930abab5f3b1659495fedd610e66fe3
Stephen <stephen@shopkeep.com>
2015-09-29 11:53:40 -0400

    untested, someone please test it

--- core/src/main/java/com/nightscout/core/upload/MongoUploader.java
+++ core/src/main/java/com/nightscout/core/upload/MongoUploader.java
@@ -16,7 +16,6 @@ import com.nightscout.core.model.MeterEntry;
 import com.nightscout.core.preferences.NightscoutPreferences;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.Date;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -49,7 +48,7 @@ public class MongoUploader extends BaseUploader {
         }
         try {
             this.client = new MongoClient(dbUri);
-        } catch (UnknownHostException e) {
+        } catch (Exception e) {
             throw new IOException("Error connecting to mongo host " + dbUri.getURI(), e);
         }
         return this.client;
@@ -150,7 +149,7 @@ public class MongoUploader extends BaseUploader {
     private boolean upsert(DBCollection collection, DBObject dbObject) {
         WriteResult result = collection.update(dbObject, dbObject, true, false,
                 WriteConcern.UNACKNOWLEDGED);
-        return result.getError() == null;
+        return result.getUpsertedId() != null;
     }
 
     @Override
